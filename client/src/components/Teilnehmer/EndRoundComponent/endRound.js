3ab37ce53b0f7568c4aa9291d0b3f5b712b7d5d4
Sebastian W <sebastianw@Sebastians-MacBook-Pro.local>
2017-11-05 17:44:06 +0100

    fix

--- client/src/components/Teilnehmer/EndRoundComponent/endRound.js
+++ client/src/components/Teilnehmer/EndRoundComponent/endRound.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import Close from 'material-ui-icons/Close';
 
 class EndRoundComponent extends Component {
 
