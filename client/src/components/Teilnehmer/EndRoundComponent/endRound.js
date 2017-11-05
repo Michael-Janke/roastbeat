ec0428013871eeb8e68fc6c6f7ef22186e904029
Oskar Schlösinger <o.schloesinger@gmx.de>
2017-11-05 18:13:01 +0100

    added endRound

--- client/src/components/Teilnehmer/EndRoundComponent/endRound.js
+++ client/src/components/Teilnehmer/EndRoundComponent/endRound.js
@@ -6,7 +6,7 @@ class EndRoundComponent extends Component {
   state = {
     name: 'paul',
     answerCorrect: false,
-    songTitle: '',
+    songTitle: 'Hey yah',
     score: 0,
     timer: 3,
   };
@@ -24,23 +24,36 @@ class EndRoundComponent extends Component {
   render() {
     const state = this.state;
 
+
+
+    function onEndClicked(e){
+      window.location.replace('/');
+    }
+
     return (
+
       <div className="">
-        <div>
-          Name: {this.props.lastPlayer}
-        </div>
-        <div>
-          rightAnswer? {this.props.answer}
-        </div>
-        <div>
-          Song title {state.songTitle}
-        </div>
-        <div>
-          Score {state.score}
-        </div>
-        <div>
-          Timer for next step {state.timer}
-        </div>
+        <h2>CURRENT  <br/>SCORES</h2>
+        <ul className="scores">
+
+          <li className="scoresItem">
+            <span className="score">{state.score}
+              <span>pts</span>
+            </span>
+            <span className="name">{state.name}</span>
+          </li>
+          <li className="scoresItem">
+            <span className="score">{state.answerCorrect ? 'Correct' : 'Wrong'}</span>
+            <span className="name">Title: {state.songTitle}</span>
+          </li>
+          <li className="scoresItem">
+            <span className="score">{state.timer}
+              <span>scnds</span>
+            </span>
+            <span className="name">Time:</span>
+          </li>
+        </ul>
+        <div className="btn_big"><div className="btn" onClick={onEndClicked.bind(this)}>close quiz</div></div>
       </div>
     );
   }
