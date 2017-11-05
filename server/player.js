f0bf57cc563634b19fe8e2cd12573aec57313b85
Michael Janke <micha@jankeweb.de>
2017-11-05 14:04:48 +0100

    first websocket func

---
+++ server/player.js
@@ -0,0 +1,8 @@
+class Player {
+    constructor() {
+        this.name = undefined;
+        this.score = 0;
+    }
+}
+
+export default Player;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
 const WebSocket = require('ws');
+import Player from './player'
+import Game from './game'
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+let games = {};
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
+  	console.log(message);
+    try {
+    	let messageObject = JSON.parse(message);
+	
+	    switch(messageObject.command) {
+	    	case "CREATE_GAME": createGame(ws);
+	    	break;
+	    	default:
+	    		console.log(messageObject.command + " command not defined")
+	    }
+  	} catch (e) {
+  		ws.send(JSON.stringify({
+	    	command: "ERROR",
+	    	error: e.message
+	    }));
+  	}
   });
-
-  ws.send('something');
 });
+
+function createGame(client) {
+	let game = new Game();
+    client.send(JSON.stringify({
+    	command: "READ_PIN",
+    	pin: game.pin
+    }));
+  	games[game.pin] = game;
+  	console.log("your pin is:" + game.pin);
+}
+
+//{"command":"CREATE_GAME"}
